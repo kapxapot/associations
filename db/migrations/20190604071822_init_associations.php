e115c3be7f990287a2f113779bbba49649140a66
Sergei Atroshchenko <sergei.atroshchenko@cbsi.com>
2019-06-04 10:26:05 +0300

    Added InitAssociations migration

---
+++ db/migrations/20190604071822_init_associations.php
@@ -0,0 +1,27 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class InitAssociations extends AbstractMigration
+{
+    public function change()
+    {
+        $table = $this->table('associations');
+
+        $table
+            ->addColumn('language_id', 'integer')
+            ->addColumn('first_word_id', 'integer')
+            ->addColumn('second_word_id', 'integer')
+            ->addColumn('word_bin', 'string', ['limit' => 250, 'collation' => 'utf8_bin'])
+            ->addColumn('created_at', 'timestamp', ['default' => 'CURRENT_TIMESTAMP'])
+            ->addColumn('created_by', 'integer', ['null' => true])
+            ->addColumn('deleted_at', 'timestamp', ['null' => true])
+            ->addColumn('deleted_by', 'integer', ['null' => true])
+            ->addForeignKey('language_id', 'languages', 'id', ['delete' => 'CASCADE', 'update' => 'CASCADE'])
+            ->addForeignKey('first_word_id', 'words', 'id', ['delete' => 'CASCADE', 'update' => 'CASCADE'])
+            ->addForeignKey('second_word_id', 'words', 'id', ['delete' => 'CASCADE', 'update' => 'CASCADE'])
+            ->addForeignKey('created_by', 'users', 'id', ['delete' => 'SET NULL', 'update' => 'CASCADE'])
+            ->addForeignKey('deleted_by', 'users', 'id', ['delete' => 'SET NULL', 'update' => 'CASCADE'])
+            ->create();
+    }
+}
