cf4970db2790478f70472950505bafc64fc26934
kapxapot <kapxapot@gmail.com>
2019-06-22 21:48:02 +0300

    Validation messages revamp

--- src/Validation/Exceptions/GameIsCurrentException.php
+++ src/Validation/Exceptions/GameIsCurrentException.php
@@ -7,7 +7,7 @@ use Respect\Validation\Exceptions\ValidationException;
 class GameIsCurrentException extends ValidationException {
 	public static $defaultTemplates = [
 		self::MODE_DEFAULT => [
-			self::STANDARD => 'Provided game is not the current game of the current user.'
+			self::STANDARD => 'Provided game is finished. Please, reload the page.'
 		]
 	];
 }
--- src/Validation/Exceptions/GameTurnIsCorrectException.php
+++ src/Validation/Exceptions/GameTurnIsCorrectException.php
@@ -7,7 +7,7 @@ use Respect\Validation\Exceptions\ValidationException;
 class GameTurnIsCorrectException extends ValidationException {
 	public static $defaultTemplates = [
 		self::MODE_DEFAULT => [
-			self::STANDARD => 'Game turn is not correct (reload the page).'
+			self::STANDARD => 'Game turn is not correct. Please, reload the page.'
 		]
 	];
 }
