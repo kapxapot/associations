57dee211ea3d118c64b20f0c7a6c7db54e4350cc
kapxapot <kapxapot@gmail.com>
2019-06-30 23:41:37 +0300

    Recount service

---
+++ src/Services/RecountService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Association;
+use App\Models\Word;
+
+class RecountService
+{
+    public function recountWordApproved(Word $word)
+    {
+        
+    }
+
+    public function recountWordMature(Word $word)
+    {
+
+    }
+
+    public function recountAssociationApproved(Association $assoc)
+    {
+
+    }
+
+    public function recountAssociationMature(Association $assoc)
+    {
+
+    }
+}
