fd3d8eb09ac7de33a3a1c659a63333cf08b013c4
kapxapot <kapxapot@gmail.com>
2019-07-14 14:38:13 +0300

    Spaces

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -21,58 +21,58 @@ class Controller extends BaseController
             $language = ($game !== null)
                 ? $game->language()
                 : $this->languageService->getDefaultLanguage();
-	    }
+        }
         
         if ($language !== null) {
-	        $wordCount = $language->words()->count();
-	        $wordCountStr = $this->cases->caseForNumber('слово', $wordCount);
+            $wordCount = $language->words()->count();
+            $wordCountStr = $this->cases->caseForNumber('слово', $wordCount);
 
-	        $associationCount = $language->associations()->count();
-	        $associationCountStr = $this->cases->caseForNumber('ассоциация', $associationCount);
+            $associationCount = $language->associations()->count();
+            $associationCountStr = $this->cases->caseForNumber('ассоциация', $associationCount);
             
             $params['language'] = $language;
             
-    	    $params = array_merge($params, [
-    	        'word_count' => $wordCount,
-    	        'word_count_str' => $wordCountStr,
-    	        'word_anniversary' => $this->isAnniversary($wordCount)
-    	            ? $this->toAnniversaryNumber($wordCount)
-    	            : null,
-    	        'association_count' => $associationCount,
-    	        'association_count_str' => $associationCountStr,
-    	        'association_anniversary' => $this->isAnniversary($associationCount)
-    	            ? $this->toAnniversaryNumber($associationCount)
-    	            : null,
+            $params = array_merge($params, [
+                'word_count' => $wordCount,
+                'word_count_str' => $wordCountStr,
+                'word_anniversary' => $this->isAnniversary($wordCount)
+                    ? $this->toAnniversaryNumber($wordCount)
+                    : null,
+                'association_count' => $associationCount,
+                'association_count_str' => $associationCountStr,
+                'association_anniversary' => $this->isAnniversary($associationCount)
+                    ? $this->toAnniversaryNumber($associationCount)
+                    : null,
             ]);
         }
         
         return parent::buildParams(['params' => $params]);
     }
-	
-	private function isAnniversary(int $num) : bool
-	{
-	    if ($num < 1000) {
-	        return false;
-	    }
-	    
-	    while ($num >= 100) {
-	        $num = intdiv($num, 10);
-	    }
-	    
-	    $rem = $num % 10;
-	    
-	    return $rem < 2;
-	}
-	
-	private function toAnniversaryNumber(int $num) : int
-	{
-	    $mult = 1;
-	    
-	    while ($num >= 10) {
-	        $num = intdiv($num, 10);
-	        $mult *= 10;
-	    }
-	    
-	    return $num * $mult;
-	}
+    
+    private function isAnniversary(int $num) : bool
+    {
+        if ($num < 1000) {
+            return false;
+        }
+        
+        while ($num >= 100) {
+            $num = intdiv($num, 10);
+        }
+        
+        $rem = $num % 10;
+        
+        return $rem < 2;
+    }
+    
+    private function toAnniversaryNumber(int $num) : int
+    {
+        $mult = 1;
+        
+        while ($num >= 10) {
+            $num = intdiv($num, 10);
+            $mult *= 10;
+        }
+        
+        return $num * $mult;
+    }
 }
