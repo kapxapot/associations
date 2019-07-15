9927ff6f5b92b1388c299382f1df8479aab7fc8c
Sergei Atroshchenko <sergei.atroshchenko@cbsi.com>
2019-07-15 17:01:13 +0300

    Another migration fix

--- db/migrations/20190715084041_init_yandex_dict_words.php
+++ db/migrations/20190715084041_init_yandex_dict_words.php
@@ -11,13 +11,13 @@ class InitYandexDictWords extends AbstractMigration
         $table
             ->addColumn('word', 'string', ['limit' => 250])
             ->addColumn('word_id', 'integer', ['null' => true])
-            ->addColumn('language_id', 'integer', ['null' => true])
+            ->addColumn('language_id', 'integer')
             ->addColumn('response', 'text', ['null' => true])
             ->addColumn('pos', 'string', ['limit' => 50, 'null' => true])
             ->addColumn('created_at', 'timestamp', ['default' => 'CURRENT_TIMESTAMP'])
             ->addColumn('updated_at', 'timestamp', ['default' => 'CURRENT_TIMESTAMP'])
             ->addForeignKey('word_id', 'words', 'id', ['delete' => 'SET_NULL', 'update' => 'CASCADE'])
-            ->addForeignKey('language_id', 'languages', 'id', ['delete' => 'SET_NULL', 'update' => 'CASCADE'])
+            ->addForeignKey('language_id', 'languages', 'id', ['delete' => 'CASCADE', 'update' => 'CASCADE'])
             ->create();
     }
 }
