05a88acdfe2b6b14958132263121b16fd79de2a0
Sergei Atroshchenko <sergei.atroshchenko@cbsi.com>
2019-07-17 11:37:36 +0300

    Spaces

--- src/Config/Config.php
+++ src/Config/Config.php
@@ -6,13 +6,13 @@ use Plasticode\Contained;
 
 class Config extends Contained
 {
-	public function wordMinLength()
-	{
-	    return $this->getSettings('view_globals.word_min_length') ?? 1;
-	}
-	
-	public function wordMaxLength()
-	{
-	    return $this->getSettings('view_globals.word_max_length') ?? 250;
-	}
+    public function wordMinLength()
+    {
+        return $this->getSettings('view_globals.word_min_length') ?? 1;
+    }
+    
+    public function wordMaxLength()
+    {
+        return $this->getSettings('view_globals.word_max_length') ?? 250;
+    }
 }
