5d0ddd6746ba5226b924d48a439ff26db71c8efe
Sergey Atroshchenko <kapxapot@gmail.com>
2021-12-01 17:44:54 +0300

    Adds normalization of original utterance

--- src/Services/WordService.php
+++ src/Services/WordService.php
@@ -131,7 +131,7 @@ class WordService
                 [
                     'language_id' => $language->getId(),
                     'word' => $wordStr,
-                    'original_utterance' => $originalUtterance,
+                    'original_utterance' => $this->normalize($originalUtterance),
                     'created_by' => $user->getId(),
                 ]
             );
